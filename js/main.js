682d8053530285b88542face115244648ac940b1
OttoMarina <otto.02@list.ru>
2022-10-08 20:29:18 +0300

    okeyyy

--- js/main.js
+++ js/main.js
@@ -1,59 +1,38 @@
-import {setFormValue, submitSignUpForm, validateEmail, validatePassword} from "./utils.js"
-
-
-////// ДЕМОНСТРАЦИОННЫЙ УЧАСТОК КОДА. На оценку не влияет, исключительно для саморазвития.
-
-// Предлагаю "поиграться" с частями кода ниже, чтобы познакомиться с JS
-// Получаем элемент и меняем его класс, который определеён в библиотеке стилей materialize
-const password = document.getElementById('password');
-password.classList.add("valid")
-password.classList.remove("valid")
-
-// В браузере можно посмотреть, что из себя представляет документ
-// (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
-console.log("Document")
-console.log(document)
-
-// Если запросить id, которого нет в DOM дереве - вернется undefined
-// => надо быть осторожней: коллега может поменять id вашего элемента и упадёт !ВАШ! код
-// const first_name = document.getElementById('first_name_invalid');
-// first_name.oninput = (e) => validatePassword(e)
-
-// Селекция по классу. Может пригодится, для того, чтобы упростить обработку полей в двух формах.
-// Чтобы не делать кучу уникальных айди, можно определённым полям формы давать один класс и обрабатывать их в цикле
-// const passwords = document.querySelectorAll('.password')
-// console.log(passwords)
-// for (const password of passwords) {
-//   password.style.background = "red"
-// }
-
-////// КОНЕЦ ДЕМОНСТРАЦИОННОГО УЧАСТКА КОДА. Дальше код для оцениваемой части задания
+import {setFormValue, submitSignUpForm, validateEmail, validatePassword, formValues, formValidation, getValidationStatus} from "./utils.js"
 
 
 // Выписываем все айдишники HTMl-элементов в константы для переиспользования
-const first_name_id = 'first_name'
-const last_name_id = 'last_name'
-const password_id = 'password'
-const email_id = 'email'
 
 const sign_in_link_id = 'sign_in_link'
+const sign_up_link_id = 'sign_up_link'
 const sign_up_form_id = 'sign_up_form'
-// const sign_in_form_id = 'sign_in_form'  // Пригодится
-const sign_up_btn_id = 'sign_up_btn'
 const sign_in_form_id = 'sign_in_form'
 
+const email= document.getElementById('email');
+const password_repeat = document.getElementById('password-repeat');
+const password = document.getElementById('password');
 
-// Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
-// oninput вызывается с параметром "event" каждый раз, когда ввод меняется
-// Значение, которое мы присваеваем этому аттрибуту - это функция, определённая в стрелочном стиле
-// Гуглить по тегам "события JS", "onchange/oninput HTML", "стрелочные функции JS", ...
 
-const first_name = document.getElementById(first_name_id);
-first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)  // Установить значение без валидации
+const names = document.querySelectorAll("#f, #last_name");
+for (const name of names)
+  name.oninput = (e) => setFormValue(name.id, e.target.value)
 
-const email = document.getElementById(email_id);
-email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail) // Установить значение с валидацией
+email.oninput = (e) => setFormValue(email.id, e.target.value, validateEmail)
 
+password.oninput = (e) => {
+  setFormValue('password', e.target.value, validatePassword)
+  if(formValidation.password)
+    password.style.borderBottom = "1px solid green";
+  else
+    password.style.borderBottom = "1px solid red";
+}
+
+password_repeat.oninput = (e) => {
+  if (e.target.value === formValues.password)
+    password_repeat.style.borderBottom = "1px solid green";
+  else
+    password_repeat.style.borderBottom = "1px solid red";
+}
 
 
 // Меняем стили объекта DOM дерева. Это позволяет скрыть форму регистрации и показать форму авторизации
@@ -64,12 +43,17 @@ switch_to_sign_in.onclick = (e) => {
   document.getElementById(sign_in_form_id).style.display = ""
 }
 
+const switch_to_sign_up = document.getElementById(sign_up_link_id);
+switch_to_sign_up.onclick = (e) => {
+  document.getElementById(sign_up_form_id).style.display = ""
+  document.getElementById(sign_in_form_id).style.display = "none"
+}
 
-const sign_up_btn = document.getElementById(sign_up_btn_id);
-sign_up_btn.onclick = (e) => {
-  // При нажатии кнопки в форме по умолчанию происходит перезагрузка страницы.
-  // Чтобы отключить его, нужно отменить стандартное поведение события
-  e.preventDefault()
-  submitSignUpForm()
+const sign_btns = document.querySelectorAll("#sign_in_btn, #sign_up_btn");
+for (const sign_btn of sign_btns) {
+  sign_btn.onclick = (e) => {
+    e.preventDefault()
+    submitSignUpForm()
+  }
 }
 
